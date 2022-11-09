bf11d37c64c1c4a23624296f67a911b091407189
eskirastali <117171759+eskirastali@users.noreply.github.com>
2022-11-09 21:59:57 +0300

    Create Odev_11.sql

---
+++ Odev_11.sql
@@ -0,0 +1,31 @@
+1.
+
+(SELECT first_name FROM actor)
+UNION 
+(SELECT first_name FROM customer);
+
+2.
+
+(SELECT first_name FROM actor)
+INTERSECT 
+(SELECT first_name FROM customer);
+
+3.
+
+(SELECT first_name FROM actor)
+EXCEPT 
+(SELECT first_name FROM customer);
+
+4.
+(SELECT last_name FROM actor)
+UNION
+(SELECT last_name FROM customer);
+
+(SELECT last_name FROM actor)
+INTERSECT
+(SELECT last_name FROM customer);
+
+(SELECT last_name FROM actor)
+EXCEPT
+(SELECT last_name FROM customer);
+
